6312777ab6bd085b1c1b52d691345baadb913c18
Melvin Tu <melvinto@gmail.com>
2017-06-18 06:59:53 +0000

    add scripts

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,10 @@
 FROM ubuntu:16.04
+MAINTAINER Melvin Tu <melvinto@gmail.com>
+
+RUN apt-get update
+RUN apt-get install -y software-properties-common
+RUN add-apt-repository ppa:max-c-lv/shadowsocks-libev
+RUN apt-get update
+RUN apt-get install -y shadowsocks-libev pwgen
+
+ENTRYPOINT ["/usr/bin/ss-server"]
---
+++ ss_kcp_setup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+PUBLIC_SERVER=127.0.0.1
+SS_BINARY=/usr/bin/ss-server
+SS_IP=0.0.0.0
+SS_PORT=8899
+SS_METHOD=aes-256-cfb
+SS_PASSWORD=`pwgen 8 1`
+
+KCP_BINARY=/usr/bin/kcp_server
+KCP_PORT=8900
+KCP_MODE=fast2
+KCP_MTU=1400
+KCP_SEND_WINDOW=1024
+KCP_RECV_WINDOW=1024
+
+while [[ $# -gt 1 ]]
+do
+key="$1"
+
+case $key in
+    -s|--server)
+    PUBLIC_SERVER="$2"
+    shift # past argument
+    ;;
+    -p|--port)
+    KCP_PORT="$2"
+    shift # past argument
+    ;;
+    -P|--ssport)
+    SS_PORT="$2"
+    shift # past argument
+    ;;
+    -k|--password)
+    SS_PASSWORD="$2"
+    ;;
+    *)
+            # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+
+$SS_BINARY -s $SS_IP -p $SS_PORT -m $SS_METHOD -k $SS_PASSWORD -v &>/var/log/ss.log &
+$KCP_BINARY -mtu $KCP_MTU -sndwnd $KCP_SEND_WINDOW -rcvwnd $KCP_RECV_WINDOW -mode $KCP_MODE -t "127.0.0.1:$SS_PORT" -l ":$KCP_PORT" --log /var/log/kcp.log &
+
+SERVER_CONFIG= <<EOF
+{
+	"from": "firewalla",
+	"server_port": $SS_PORT,
+	"password":"$SS_PASSWORD",
+	"timeout":1000,
+	"method":"aes-256-cfb",
+	"server": "$PUBLIC_SERVER",
+	"kcp_enabled": true,
+	"kcp_mode": "$KCP_MODE",
+	"kcp_send_window": $KCP_SEND_WINDOW,
+	"kcp_recv_window": $KCP_RECV_WINDOW,
+	"kcp_server": "$PUBLIC_SERVER",
+	"kcp_server_port": "$KCP_PORT"
+}
+
+EOF
+
+QR_BINARY=/usr/bin/qrcode-terminal
+$QR_BINARY $SERVER_CONFIG
